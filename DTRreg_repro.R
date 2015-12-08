2dc646fa22349ac590ce5ba62a7c5ae0bc3b2cd4
Gabrielle Simoneau <gabrielle.simoneau@gmail.com>
2015-12-08 15:21:41 -0500

    add example 4.2

--- DTRreg_repro.R
+++ DTRreg_repro.R
@@ -1,6 +1,6 @@
 library(DTRreg)
 
-################### simulated example from "Introducing R package DTRreg" ###################
+################### simulated example 4.1 from "Introducing R package DTRreg" ###################
 set.seed(1)
 expit <- function(x){1/(1+exp(-x))}
 n <- 10000
@@ -18,6 +18,24 @@ tf.mod <- list(~x1,~x2)
 weight.fun <- function(w){1/w}
 
 obj=DTRreg(y,blip.mod,treat.mod,tf.mod, method="dwols",weight=weight.fun)
+summary(obj)
+
+################### simulated example 4.2 from "Introducing R package DTRreg" ###################
+set.seed(1)
+n <- 10000
+x1 <- rnorm(n)
+x2 <- rnorm(n)
+a1 <- rnorm(n, x1, 0.5)
+a2 <- rnorm(n, x2, 0.5)
+gamma1 <- a1 * (1 + x1 - a1)
+gamma2 <- a2 * (1 + x2 - a2)
+y <- exp(x1) + exp(x2) + gamma1 + gamma2 + rnorm(n)
+
+blip.mod <- list(~ x1 + a1, ~ x2 + a2)
+treat.mod <- list(a1 ~ x1, a2 ~ x2)
+tf.mod <- list( ~ x1 ,~ x1 + a1 + x2)
+
+obj=DTRreg(y,blip.mod,treat.mod,tf.mod,var.estim = "bootstrap", B = 200, treat.range = c(-5,5))
 summary(obj)
 
 ############################ simulation result in Table 1 ################################# 
